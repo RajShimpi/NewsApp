5a96fc5b4c61136b75ebc6570402153e5adf0277
RajShimpi <rajshimpi8812@gmail.com>
2023-09-21 12:38:08 +0530

    some changes

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import loading from './loading.gif'
+import loadingGif from './loading.gif';
+
 
 const Spinner = ()=> {
         return (
             <div className="text-center">
-                <img className="my-3" src={loading} alt="loading" />
+                <img className="my-3" src={loadingGif} alt="loading" />
             </div>
         )
 }
